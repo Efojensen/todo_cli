81493689c701b8fc2f12986d4d028b2b48440a76
EfoJensen <Jensena231@gmail.com>
2025-04-17 18:30:38 +0000

    Finished the go todo CLI app

--- main.go
+++ main.go
@@ -4,10 +4,6 @@ func main() {
 	todos := Todos{}
 	storage := NewStorage[Todos]("todos.json")
 	storage.Load(&todos)
-	todos.add("Go to school")
-	todos.add("Get a job")
-	todos.add("Drink whisky once")
-	todos.toggleCompleted(1)
-	todos.print()
-	storage.Save(todos)
+	cmdFlags := NewCmdFlags()
+	cmdFlags.Execute(&todos)
 }
