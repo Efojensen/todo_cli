b0b096e9a995e54628964004a85cb14ea7532ad5
EfoJensen <Jensena231@gmail.com>
2025-04-17 01:04:10 +0000

    Made storage from go code

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 func main() {
 	todos := Todos{}
+	storage := NewStorage[Todos]("todos.json")
+	storage.Load(&todos)
 	todos.add("Go to school")
 	todos.add("Get a job")
 	todos.add("Drink whisky once")
 	todos.toggleCompleted(1)
 	todos.print()
-	todos.delete(2)
+	storage.Save(todos)
 }
